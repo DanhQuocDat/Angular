b5db7a9e08dac535583a035c77af6662726f8018
QuocDat <datb1706798@gmail.com>
2022-05-15 23:44:26 +0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './interface/user';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'video-game-db';
+export class AppComponent implements OnInit{
+
+  private newUser: any = {
+    'id': 3,
+    'name': 'New Leanne Graham',
+    'username': 'Bret',
+    'email': 'Sincere@april.biz.NEW',
+  }
+  
+  constructor(private userService: UserService){ }
+
+  ngOnInit(): void {
+    // this.onUpdatePatchUser();
+    this.onGetUsers();
+    // this.onGetUser();
+    // this.onCreateUser();
+    // this.onUpdateUser();
+    this.deleteUser();
+  }
+
+  onGetUsers(): void{
+    this.userService.getUsers().subscribe(
+      (respone) => { console.table(respone)},
+      (err: any) => { console.log(err)},
+      () => { console.log('Done Getting Users')},
+    );
+  }
+
+  onGetUser(): void{
+    this.userService.getUser().subscribe(
+      (respone) => { console.log(respone)},
+      (err: any) => { console.log(err)},
+      () => { console.log('Done Getting User')},
+    );
+  }
+
+  onCreateUser(): void{
+    this.userService.createUser(this.newUser).subscribe(
+      (respone) => { console.log(respone) },
+      (err) => { console.log(err) },
+      () => { console.log('Done Creating New User') },
+    );
+  }
+
+  onUpdateUser(): void{
+    this.userService.updateUser(this.newUser).subscribe(
+      (respone) => { console.log(respone) },
+      (err) => { console.log(err) },
+      () => { console.log('Done Updating User') },
+    );
+  }
+
+  onUpdatePatchUser(): void {
+    this.userService.patchUser(this.newUser).subscribe(
+      (respone) => { console.log(respone) },
+      (err) => { console.log(err) },
+      () => { console.log('Done Updating With Option Field') },
+    );
+  }
+
+  deleteUser(): void {
+    this.userService.deleteUser(5).subscribe(
+      (respone) => { console.log(respone) },
+      (err) => { console.log(err) },
+      () => { console.log('Done Deleting User') },
+    );
+  }
 }
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interface/user';
+import { environment as env } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiURL = env.apiURL;
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]>{
+    const myParam = new HttpParams().set('page', '3').set('sort', 'true');
+    return this.http.get<User[]>(`${this.apiURL}/users`, { params: myParam })
+  }
+
+  getUser(): Observable<User>{
+    return this.http.get<User>(`${this.apiURL}/users/1`)
+  }
+
+  createUser(user: User): Observable<User>{
+    return this.http.post<User>(`${this.apiURL}/users`, user)
+  }
+
+  // UPDATE all field
+  updateUser(user: User): Observable<User>{
+    return this.http.put<User>(`${this.apiURL}/users/${user.id}`, user)
+  }
+
+  // UPDATE with OPTION Field
+  patchUser(user: User): Observable<User>{
+    return this.http.patch<User>(`${this.apiURL}/users/${user.id}`, user)
+  }
+
+  //Detele User Data
+  deleteUser(id: number): Observable<unknown>{
+    return this.http.delete<unknown>(`${this.apiURL}/users/${id}`);
+  }
+}
